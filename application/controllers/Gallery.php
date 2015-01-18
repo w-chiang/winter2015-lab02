10ea32b542863733ccc94602678f5eea1294631e
w-chiang <williampwc@gmail.com>
2015-01-18 12:46:43 -0800

    added codeigniter3 to the project added 2 additional controllers for about and gallery pages Signed-off-by:w-chiang <williampwc@gmail.com>

---
+++ application/controllers/Gallery.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Gallery
+ *
+ * @author Weary Yam
+ */
+class Gallery extends CI_Controller {   
+    public function gallery()
+    {
+            $this->load->view('gallery');
+    }
+    //put your code here
+}
